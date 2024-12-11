ab765bbc2a6d1333e3ac54c5ff4306eb3f24d48b
dancarpenter21 <dancarpenter21@gmail.com>
2024-12-10 21:37:21 -0600

    Chapter 2 guessing game complete

--- chapter2_guessing_game/src/main.rs
+++ chapter2_guessing_game/src/main.rs
@@ -1,14 +1,42 @@
 use std::io;
+use rand::Rng;
+use std::cmp::Ordering;
 
 fn main() {
     println!("Guess a number between 1 and 100!");
 
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+    //println!("Target {secret_number}");
 
-    println!("Input your guess ...");
+    loop {
+        println!("Input your guess ...");
 
-    let mut guess = String::new();
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess);
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("IO error reading user input");
 
-    println!("You guessed: {guess}");
+        // note the 'match' call here that sends us down
+        // the [Ok, Err] branches
+        let guess: u32 = match guess.trim().parse() {
+            Ok(num) => num,
+            Err(_) => {
+                println!("Invalid guess");
+                continue;
+            }
+        };
+
+        println!("You guessed: {guess}");
+
+        let _ = match guess.cmp(&secret_number) {
+            Ordering::Less    => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal   => {
+                println!("You win!");
+                break;
+            }
+        };  // this ';' must be here if we use let x = match ...
+    }
+
 }
