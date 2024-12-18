590825e0eab2d47167d055e56e1f1ef93fcdf9f9
Daniel Carpenter <danca@DESKTOP-5LB1VNC>
2024-12-18 13:50:09 -0500

    Completed chapter 6 and initiated chapter 7

--- README.md
+++ README.md
@@ -37,3 +37,13 @@ Learned about references and borrowing. Use { } to manipulate scope if needed.
  Completed chapter 5:
 
  Structs let you create custom types that are meaningful for your domain. By using structs, you can keep associated pieces of data connected to each other and name each piece to make your code clear. In impl blocks, you can define functions that are associated with your type, and methods are a kind of associated function that let you specify the behavior that instances of your structs have.
+
+ Completed chapter 6:
+
+ We’ve now covered how to use enums to create custom types that can be one of a set of enumerated values. We’ve shown how the standard library’s Option<T> type helps you use the type system to prevent errors. When enum values have data inside them, you can use match or if let to extract and use those values, depending on how many cases you need to handle.
+
+Your Rust programs can now express concepts in your domain using structs and enums. Creating custom types to use in your API ensures type safety: the compiler will make certain your functions only get values of the type each function expects.
+
+In order to provide a well-organized API to your users that is straightforward to use and only exposes exactly what your users will need, let’s now turn to Rust’s modules.
+
+'if let' seems weird, but not super important.
--- chapter6/src/main.rs
+++ chapter6/src/main.rs
@@ -1,3 +1,123 @@
+// basic enum
+enum IpAddressKind {
+    V4,
+    V6,
+}
+
+// typed enum
+enum IpAddressType {
+    V4(String),
+    V6(String),
+}
+
+// more flexible
+enum IpAddr {
+    V4(u8, u8, u8, u8),
+    V6(String),
+}
+
+enum Message {
+    Quit,                           // has no data associated
+    Move { x: i32, y: i32 },        // has two named fields like a struct
+    Write(String),                  // has a single string
+    ChangeColor(i32, i32, i32),     // as 3 i32 for RGB
+}
+
+impl Message {
+    fn call(&self) {
+        // can define methods on enum types
+        println!("Called message");
+    }
+}
+
+enum Coin {
+    Penny,
+    Nickel,
+    Dime,
+    Quarter(UsState),
+}
+
+#[derive(Debug)]
+enum UsState {
+    Delaware,
+    Pennsylvania,
+    Virginia,
+    New_York,
+    Massachussets,
+    Maryland,
+}
+
 fn main() {
-    println!("Hello, world!");
+    println!("Chapter 6 Enums and Pattern Matching");
+
+    section6_1();
+
+    section6_2();
+
+    section6_3();
+}
+
+fn section6_1() {
+    println!("Section 6.1 Defining an Enum");
+    let four = IpAddressKind::V4;
+    let six = IpAddressKind::V6;
+
+    let home = IpAddressType::V4(String::from("127.0.0.1"));
+    let loopback = IpAddressType::V6(String::from("::1"));
+
+    let home = IpAddr::V4(127,0,0,1);
+    let loopback = IpAddr::V6(String::from("::1"));
+
+    let m = Message::Write(String::from("hi there"));
+    m.call();
+}
+
+fn route(ip_kind: IpAddressKind) { }
+
+fn section6_2() {
+    println!("Section 6.2 Control Flow - match");
+    
+    let value = value_in_cents(Coin::Penny);
+    println!("{value}");
+
+    let value = value_in_cents(Coin::Quarter(UsState::New_York));
+    println!("{value}");
+
+    let five = Some(5);
+    let six = plus_one(five);
+    println!("{}", six.unwrap());
+}
+
+fn value_in_cents(coin: Coin) -> u8 {
+    match coin {
+        Coin::Penny => {
+            println!("Lucky penny!");
+            1
+        },
+        Coin::Nickel => 5,
+        Coin::Dime => 10,
+        Coin::Quarter(state) => {   // add the 'state' variable to build to the UsState held here
+            println!("State quarter of {state:?}");
+            25
+        },
+    }
+}
+
+fn plus_one(x: Option<i32>) -> Option<i32> {
+    match x {
+        None => x,
+        Some(i) => {
+            Some(i+1)
+        },
+    }
+}
+
+fn section6_3() {
+    // this is not assignment despite the '=' sign
+    println!("Section 6.3 Concise Flow with 'let if'");
+
+    let config_max = Some(3u8);
+    if let Some(max) = config_max {
+        println!("The max is {max}");
+    }
 }
